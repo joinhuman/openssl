d0f4b758a5188e928218f7a67bd477cf9750c415
Dmitriy Park <trifid@gmail.com>
2023-09-06 19:30:15 +0400

    fix go sum

--- build.go
+++ build.go
@@ -19,7 +19,7 @@ package openssl
 
 // #cgo linux windows pkg-config: libssl libcrypto
 // #cgo linux CFLAGS: -Wno-deprecated-declarations
-// #cgo darwin CFLAGS: -I/usr/local/opt/openssl@3.1/include -I/usr/local/opt/openssl/include -Wno-deprecated-declarations
-// #cgo darwin LDFLAGS: -L/usr/local/opt/openssl@3.1/lib -L/usr/local/opt/openssl/lib -lssl -lcrypto
+// #cgo darwin CFLAGS: -I/usr/local/opt/openssl@3.1/include -I/usr/local/opt/openssl/include -I/Users/dpak/ws/openssl/include -Wno-deprecated-declarations
+// #cgo darwin LDFLAGS: -L/usr/local/opt/openssl@3.1/lib -L/usr/local/opt/openssl/lib -L/Users/dpak/ws/openssl -lssl -lcrypto
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
 import "C"
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/joinhuman/openssl
 
-require github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572
+go 1.20
+
+require (
+	github.com/spacemonkeygo/openssl v0.0.0-20181017203307-c2dcc5cca94a
+	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572
+)
+
+require golang.org/x/sys v0.12.0 // indirect

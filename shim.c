ac69937cbe8f90a8271cdc5cf71e844b99f87c85
Dmitriy Park <trifid@gmail.com>
2023-09-06 20:01:47 +0400

    remove crypto/evp.h dependency

--- shim.c
+++ shim.c
@@ -25,8 +25,111 @@
 #include <openssl/err.h>
 #include <openssl/evp.h>
 #include <openssl/ssl.h>
-#include <crypto/evp.h>
+//#include <crypto/evp.h>
 #include <openssl/types.h>
+
+struct evp_pkey_ctx_st {
+    /* Actual operation */
+    int operation;
+
+    /*
+     * Library context, property query, keytype and keymgmt associated with
+     * this context
+     */
+    OSSL_LIB_CTX *libctx;
+    char *propquery;
+    const char *keytype;
+    /* If |pkey| below is set, this field is always a reference to its keymgmt */
+    EVP_KEYMGMT *keymgmt;
+
+    union {
+        struct {
+            void *genctx;
+        } keymgmt;
+
+        struct {
+            EVP_KEYEXCH *exchange;
+            /*
+             * Opaque ctx returned from a providers exchange algorithm
+             * implementation OSSL_FUNC_keyexch_newctx()
+             */
+            void *algctx;
+        } kex;
+
+        struct {
+            EVP_SIGNATURE *signature;
+            /*
+             * Opaque ctx returned from a providers signature algorithm
+             * implementation OSSL_FUNC_signature_newctx()
+             */
+            void *algctx;
+        } sig;
+
+        struct {
+            EVP_ASYM_CIPHER *cipher;
+            /*
+             * Opaque ctx returned from a providers asymmetric cipher algorithm
+             * implementation OSSL_FUNC_asym_cipher_newctx()
+             */
+            void *algctx;
+        } ciph;
+        struct {
+            EVP_KEM *kem;
+            /*
+             * Opaque ctx returned from a providers KEM algorithm
+             * implementation OSSL_FUNC_kem_newctx()
+             */
+            void *algctx;
+        } encap;
+    } op;
+
+    /*
+     * Cached parameters.  Inits of operations that depend on these should
+     * call evp_pkey_ctx_use_delayed_data() when the operation has been set
+     * up properly.
+     */
+    struct {
+        /* Distinguishing Identifier, ISO/IEC 15946-3, FIPS 196 */
+        char *dist_id_name; /* The name used with EVP_PKEY_CTX_ctrl_str() */
+        void *dist_id;      /* The distinguishing ID itself */
+        size_t dist_id_len; /* The length of the distinguishing ID */
+
+        /* Indicators of what has been set.  Keep them together! */
+        unsigned int dist_id_set : 1;
+    } cached_parameters;
+
+    /* Application specific data, usually used by the callback */
+    void *app_data;
+    /* Keygen callback */
+    EVP_PKEY_gen_cb *pkey_gencb;
+    /* implementation specific keygen data */
+    int *keygen_info;
+    int keygen_info_count;
+
+    /* Legacy fields below */
+
+    /* EVP_PKEY identity */
+    int legacy_keytype;
+    /* Method associated with this operation */
+    const EVP_PKEY_METHOD *pmeth;
+    /* Engine that implements this method or NULL if builtin */
+    ENGINE *engine;
+    /* Key: may be NULL */
+    EVP_PKEY *pkey;
+    /* Peer key for key agreement, may be NULL */
+    EVP_PKEY *peerkey;
+    /* Algorithm specific data */
+    void *data;
+    /* Indicator if digest_custom needs to be called */
+    unsigned int flag_call_digest_custom:1;
+    /*
+     * Used to support taking custody of memory in the case of a provider being
+     * used with the deprecated EVP_PKEY_CTX_set_rsa_keygen_pubexp() API. This
+     * member should NOT be used for any other purpose and should be removed
+     * when said deprecated API is excised completely.
+     */
+    BIGNUM *rsa_pubexp;
+} /* EVP_PKEY_CTX */ ;
 
 #include "_cgo_export.h"
 
